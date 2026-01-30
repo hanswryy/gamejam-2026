35d85ca2a4413071e5f3870ca2ae52c6c8bb4d03
Muhamad Haidar Al-Farruq <muharuichijou@gmail.com>
2026-01-30 22:31:17 +0700

    Add: Enemy Follower

---
+++ Assets/Scripts/Enemy/EnemyFollower.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollower : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] int damage;
+    GameObject player;
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        player = GameObject.FindWithTag("Player");
+        ProcessDirection();
+    }
+
+    void FixedUpdate()
+    {
+        ProcessRotation();
+    }
+
+    void ProcessDirection()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    void ProcessRotation()
+    {
+        Vector3 targetPosition = player.transform.position - transform.position;
+        targetPosition.y = 0; // Cap Y-axis rotation
+        Quaternion rotation = Quaternion.LookRotation(targetPosition);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
+    }
+}
